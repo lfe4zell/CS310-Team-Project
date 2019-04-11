7734dd6d1e9dcf7b7ce7c27e70bef64f55ae3b4c
Paul Brasfield <pbrasfield@stu.jsu.edu>
2019-04-11 15:08:01 -0500

    Updated code for invalid pairs of element strings

--- src/cs310/team/project/GS1_8X.java
+++ src/cs310/team/project/GS1_8X.java
@@ -12,50 +12,50 @@ public class GS1_8X extends GS1 {
         super.matchers = new HashMap<>();
         
         super.matchers.put("8001" , Pattern.compile("^8001[0-9]{14}[%\\x1D]"));
-        super.matchers.put("8002" , Pattern.compile("^8002(.){1,20}[%\\x1D]"));
-        super.matchers.put("8003" , Pattern.compile("^8003[0-9]{14}[0-9]{4}(.){1,16}[%\\x1D]"));
-        super.matchers.put("8004" , Pattern.compile("^8004(.){1,30}[%\\x1D]"));
+        super.matchers.put("8002" , Pattern.compile("^8002[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,20}[%\\x1D]"));
+        super.matchers.put("8003" , Pattern.compile("^8003[0-9]{14}[0-9]{4}[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,16}[%\\x1D]"));
+        super.matchers.put("8004" , Pattern.compile("^8004[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,30}[%\\x1D]"));
         super.matchers.put("8005" , Pattern.compile("^8005[0-9]{6}[%\\x1D]"));
         super.matchers.put("8006" , Pattern.compile("^8006[0-9]{4}[0-9]{14}[0-9]{2}[0-9]{2}[%\\x1D]"));
-        super.matchers.put("8007" , Pattern.compile("^8007(.){1,34}[%\\x1D]"));
+        super.matchers.put("8007" , Pattern.compile("^8007[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,34}[%\\x1D]"));
         super.matchers.put("8008" , Pattern.compile("^8008[0-9]{4}[0-9]{8}[0-9]{1,4}[%\\x1D]"));
-        super.matchers.put("8009" , Pattern.compile("^8009(.){1,50}[%\\x1D]"));
-        super.matchers.put("8010" , Pattern.compile("^8010(.){1,30}[%\\x1D]"));
+        super.matchers.put("8009" , Pattern.compile("^8009[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,50}[%\\x1D]"));
+        super.matchers.put("8010" , Pattern.compile("^8010[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,30}[%\\x1D]"));
         super.matchers.put("8011" , Pattern.compile("^8011[0-9]{1,12}[%\\x1D]"));
-        super.matchers.put("8012" , Pattern.compile("^8012(.){1,20}[%\\x1D]"));
-        super.matchers.put("8013" , Pattern.compile("^8013(.){1,30}[%\\x1D]"));
+        super.matchers.put("8012" , Pattern.compile("^8012[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,20}[%\\x1D]"));
+        super.matchers.put("8013" , Pattern.compile("^8013[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,30}[%\\x1D]"));
         super.matchers.put("8017" , Pattern.compile("^8017[0-9]{18}[%\\x1D]"));
         super.matchers.put("8018" , Pattern.compile("^8018[0-9]{18}[%\\x1D]"));
         super.matchers.put("8019" , Pattern.compile("^8019[0-9]{1,10}[%\\x1D]"));
-        super.matchers.put("8020" , Pattern.compile("^8020(.){1,25}[%\\x1D]"));
+        super.matchers.put("8020" , Pattern.compile("^8020[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,25}[%\\x1D]"));
         super.matchers.put("8026" , Pattern.compile("^8026[0-9]{4}[0-9]{14}[0-9]{2}[0-9]{2}[%\\x1D]"));
-        super.matchers.put("8110" , Pattern.compile("^8110(.){1,70}[%\\x1D]"));
+        super.matchers.put("8110" , Pattern.compile("^8110[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,70}[%\\x1D]"));
         super.matchers.put("8111" , Pattern.compile("^8111[0-9]{4}[%\\x1D]"));
-        super.matchers.put("8112" , Pattern.compile("^8112(.){1,70}[%\\x1D]"));
-        super.matchers.put("8200" , Pattern.compile("^8200(.){1,70}[%\\x1D]"));
+        super.matchers.put("8112" , Pattern.compile("^8112[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,70}[%\\x1D]"));
+        super.matchers.put("8200" , Pattern.compile("^8200[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,70}[%\\x1D]"));
         
         super.matchers.put("8001eol" , Pattern.compile("^8001[0-9]{14}[%\\x1D]"));
-        super.matchers.put("8002eol" , Pattern.compile("^8002(.){1,20}$"));
-        super.matchers.put("8003eol" , Pattern.compile("^8003[0-9]{14}[0-9]{4}(.){1,16}$"));
-        super.matchers.put("8004eol" , Pattern.compile("^8004(.){1,30}$"));
+        super.matchers.put("8002eol" , Pattern.compile("^8002[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,20}$"));
+        super.matchers.put("8003eol" , Pattern.compile("^8003[0-9]{14}[0-9]{4}[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,16}$"));
+        super.matchers.put("8004eol" , Pattern.compile("^8004[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,30}$"));
         super.matchers.put("8005eol" , Pattern.compile("^8005[0-9]{6}$"));
         super.matchers.put("8006eol" , Pattern.compile("^8006[0-9]{4}[0-9]{14}[0-9]{2}[0-9]{2}$"));
-        super.matchers.put("8007eol" , Pattern.compile("^8007(.){1,34}$"));
+        super.matchers.put("8007eol" , Pattern.compile("^8007[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,34}$"));
         super.matchers.put("8008eol" , Pattern.compile("^8008[0-9]{4}[0-9]{8}[0-9]{1,4}$"));
-        super.matchers.put("8009eol" , Pattern.compile("^8009(.){1,50}$"));
-        super.matchers.put("8010eol" , Pattern.compile("^8010(.){1,30}$"));
+        super.matchers.put("8009eol" , Pattern.compile("^8009[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,50}$"));
+        super.matchers.put("8010eol" , Pattern.compile("^8010[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,30}$"));
         super.matchers.put("8011eol" , Pattern.compile("^8011[0-9]{1,12}$"));
-        super.matchers.put("8012eol" , Pattern.compile("^8012(.){1,20}$"));
-        super.matchers.put("8013eol" , Pattern.compile("^8013(.){1,30}$"));
+        super.matchers.put("8012eol" , Pattern.compile("^8012[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,20}$"));
+        super.matchers.put("8013eol" , Pattern.compile("^8013[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,30}$"));
         super.matchers.put("8017eol" , Pattern.compile("^8017[0-9]{18}$"));
         super.matchers.put("8018eol" , Pattern.compile("^8018[0-9]{18}$"));
         super.matchers.put("8019eol" , Pattern.compile("^8019[0-9]{1,10}$"));
-        super.matchers.put("8020eol" , Pattern.compile("^8020(.){1,25}$"));
+        super.matchers.put("8020eol" , Pattern.compile("^8020[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,25}$"));
         super.matchers.put("8026eol" , Pattern.compile("^8026[0-9]{4}[0-9]{14}[0-9]{2}[0-9]{2}$"));
-        super.matchers.put("8110eol" , Pattern.compile("^8110(.){1,70}$"));
+        super.matchers.put("8110eol" , Pattern.compile("^8110[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,70}$"));
         super.matchers.put("8111eol" , Pattern.compile("^8111[0-9]{4}$"));
-        super.matchers.put("8112eol" , Pattern.compile("^8112(.){1,70}$"));
-        super.matchers.put("8200eol" , Pattern.compile("^8200(.){1,70}$"));    
+        super.matchers.put("8112eol" , Pattern.compile("^8112[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,70}$"));
+        super.matchers.put("8200eol" , Pattern.compile("^8200[!\"&'()*+,\\-./0-9:;<=>?A-Z_a-z]{1,70}$"));    
     }
     
     public Object parse8001(String element) {
@@ -64,21 +64,23 @@ public class GS1_8X extends GS1 {
         
         output.put("title", "DIMENSIONS");
         output.put("ai", element.substring(0, 4));
+        output.put("data", element.substring(4));
         output.put("width", element.substring(4, 8));
         output.put("length", element.substring(8,13));
         output.put("diameter", element.substring(13, 16));
         
-        if (element.substring(16,17).equals(0)) {
+        if (element.substring(16,17).equals("0")) {
             output.put("winding direction", "face out");}
-        else if (element.substring(16,17).equals(1)) {
+        else if (element.substring(16,17).equals("1")) {
             output.put("winding direction", "face in");}   
         else {
             output.put("winding direction", "undefined");}
         
-        if (element.substring(17,18).equals(9)) {
+        if (element.substring(17,18).equals("9")) {
             output.put("splices", "number unkown");}
         else {
             output.put("splices", element.charAt(17));}
+        
         
         output.put("element", element);
         
@@ -111,6 +113,7 @@ public class GS1_8X extends GS1 {
         
         output.put("title", "GRAI");
         output.put("ai", element.substring(0,4));
+        output.put("data", element.substring(4));
         output.put("GS1 Company Prefix", element.substring(4,18));
         output.put("Check digit", element.charAt(18));
         output.put("Serial component", element.substring(19));
@@ -161,6 +164,7 @@ public class GS1_8X extends GS1 {
         
         output.put("title", "ITIP or GCTIN");
         output.put("ai", element.substring(0,4));
+        output.put("data", element.substring(4));
         output.put("GTIN", element.substring(4,18));
         output.put("piece number", element.substring(18,20));
         output.put("total count", element.substring(20,22));
@@ -195,9 +199,10 @@ public class GS1_8X extends GS1 {
         
         output.put("title", "PROD TIME");
         output.put("ai", element.substring(0,4));
+        output.put("data", element.substring(4));
         output.put("year", element.substring(4,6));
         output.put("month", element.substring(6,8));
-        output.put("dfay", element.substring(8,10));
+        output.put("day", element.substring(8,10));
         output.put("hour", element.substring(10,12));
         output.put("minutes", element.substring(12,14));
         output.put("seconds", element.substring(14,16));
@@ -296,6 +301,7 @@ public class GS1_8X extends GS1 {
         
         output.put("title", "GSRN-PROVIDER");
         output.put("ai", element.substring(0,4));
+        output.put("data", element.substring(4));
         output.put("GSRN Provider", element.substring(4, 21));
         output.put("Check digit", element.charAt(21));
         output.put("element", element);
@@ -313,6 +319,7 @@ public class GS1_8X extends GS1 {
         
         output.put("title", "GSRN-RECIPIENT");
         output.put("ai", element.substring(0,4));
+        output.put("data", element.substring(4));
         output.put("GSRN Recipient", element.substring(4,21));
         output.put("Chech digit", element.charAt(21));
         output.put("element", element);
@@ -362,6 +369,7 @@ public class GS1_8X extends GS1 {
         
         output.put("title", "ITIP CONTENT");
         output.put("ai", element.substring(0,4));
+        output.put("data", element.substring(4));
         output.put("GTIN", element.substring(4, 18));
         output.put("piece number", element.substring(18, 20));
         output.put("total count", element.substring(20, 22));
@@ -380,7 +388,7 @@ public class GS1_8X extends GS1 {
         
         output.put("title", "COUPON CODE");
         output.put("ai", element.substring(0,4));
-        output.put("couon code", element.substring(4));
+        output.put("coupon code", element.substring(4));
         output.put("element", element);
                 
         return output;      
